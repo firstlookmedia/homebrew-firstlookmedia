e9f03a3a209ce64c839b2e76d7338f12fd2cad20
Micah Lee <micah@micahflee.com>
2019-04-25 17:47:19 -0700

    flock-agent 0.0.4

--- Casks/flock-agent.rb
+++ Casks/flock-agent.rb
@@ -1,6 +1,6 @@
 cask 'flock-agent' do
-  version '0.0.3'
-  sha256 '499ea29cfed1d28f168a52042193f39e5d1d098855331f075e30f7c4eb74ecfd'
+  version '0.0.4'
+  sha256 'ccf69acba6cbd37e529070e2f1b2be24a15cce91667c64dd15786451f4582e3d'
 
   url "https://github.com/firstlookmedia/flock-agent/releases/download/v#{version}/FlockAgent-#{version}.pkg"
   appcast 'https://github.com/firstlookmedia/flock-agent/releases.atom'
