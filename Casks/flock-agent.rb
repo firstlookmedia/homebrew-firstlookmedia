b6b2da965075de8b72dd6263c9e1df4d78d2493c
Micah Lee <micah@micahflee.com>
2019-10-21 11:14:37 -0700

    flock-agent 0.0.10

--- Casks/flock-agent.rb
+++ Casks/flock-agent.rb
@@ -1,6 +1,6 @@
 cask 'flock-agent' do
-  version '0.0.9'
-  sha256 '51495a30df7c23497df1fbfc7cfdefc3a1280a9981992bfd345b2650fab4d90d'
+  version '0.0.10'
+  sha256 '78aa1cf9917d4fdb498ef015bf7948b48a53a4e3d0ce9ab4fa61df19941b86ce'
 
   url "https://github.com/firstlookmedia/flock-agent/releases/download/v#{version}/FlockAgent-#{version}.pkg"
   appcast 'https://github.com/firstlookmedia/flock-agent/releases.atom'
@@ -10,9 +10,9 @@ cask 'flock-agent' do
   pkg "FlockAgent-#{version}.pkg"
 
   uninstall quit:      'media.firstlook.flock-agent',
-            pkgutil:   'media.firstlook.flock_agent',
+            pkgutil:   'media.firstlook.flock-agent',
             pkgutil:   'com.facebook.osquery',
-            launchctl: 'media.firstlook.flock_agent',
+            launchctl: 'media.firstlook.flock-agent',
             delete:    '/Library/LaunchDaemons/com.facebook.osqueryd.plist',
             delete:    '/Library/LaunchDaemons/media.firstlook.flock-agentd.plist',
             delete:    '/private/var/log/osquery',
